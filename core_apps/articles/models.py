3145ca1b2b0541be159e01d6eff0be8f9883e30f
Benjamin Idewor <benjaminidewor@gmail.com>
2023-05-27 08:41:13 +0100

    feat: article clap

--- core_apps/articles/models.py
+++ core_apps/articles/models.py
@@ -11,6 +11,17 @@ from .read_time_engine import ArticleReadTimeEngine
 User = get_user_model()
 
 
+class Clap(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["user", "article"]
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} clapped {self.article.title}"
+
+
 class Article(TimeStampedModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
     title = models.CharField(verbose_name=_("Title"), max_length=255)
@@ -21,6 +32,7 @@ class Article(TimeStampedModel):
         verbose_name=_("banner image"), default="/profile_default.png"
     )
     tags = TaggableManager()
+    claps = models.ManyToManyField(User, through=Clap, related_name="clapped_articles")
 
     def __str__(self) -> str:
         return f"{self.author.first_name}'s article"
--- core_apps/articles/serializers.py
+++ core_apps/articles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core_apps.articles.models import Article, ArticleView
+from core_apps.articles.models import Article, ArticleView, Clap
 from core_apps.bookmarks.models import Bookmark
 from core_apps.bookmarks.serializers import BookmarkSerializer
 from core_apps.profiles.serializers import ProfileSerializer
@@ -33,6 +33,7 @@ class ArticleSerializer(serializers.ModelSerializer):
     average_rating = serializers.ReadOnlyField()
     bookmarks = serializers.SerializerMethodField()
     bookmarks_count = serializers.SerializerMethodField()
+    claps_count = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
 
@@ -58,6 +59,9 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     def get_bookmarks_count(self, obj):
         return Bookmark.objects.filter(article=obj).count()
+
+    def get_claps_counts(self, obj):
+        return Clap.objects.filter(article=obj).count()
 
     def get_average_rating(self, obj):
         return obj.average_rating()
@@ -100,6 +104,16 @@ class ArticleSerializer(serializers.ModelSerializer):
             "average_rating",
             "bookmarks",
             "bookmarks_count",
+            "claps_count",
             "created_at",
             "updated_at",
         ]
+
+
+class ClapSerializer(serializers.ModelSerializer):
+    article_title = serializers.CharField(source="article.title", read_only=True)
+    user_first_name = serializers.CharField(source="user.first_name", read_only=True)
+
+    class Meta:
+        model = Clap
+        fields = ["id", "user_first_name", "article_title"]
